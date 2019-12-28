49d659d20da0d00939e7ef4852cf0ec68053b3a1
Vladimir <timos9vs@gmail.com>
2019-12-28 09:48:53 +0200

    add tikets controller spec

--- spec/controllers/tikets_controller_spec.rb
+++ spec/controllers/tikets_controller_spec.rb
@@ -18,4 +18,33 @@ RSpec.describe TiketsController, type: :controller do
     end
   end
 
+  context 'index' do
+    let(:tiket){ create(:tiket) }
+
+    it 'renders the :index template' do
+      get :index
+      expect(response).to have_http_status(200)
+      expect(response).to render_template('index')
+    end
+
+    it 'assigns the category' do
+      get :index
+      expect(assigns(:tikets)).to include(tiket)
+    end
+  end
+
+  context 'show' do
+    let(:tiket){ create(:tiket) }
+
+    it 'renders the :index template' do
+      get :show, params: { id: tiket.id }
+      expect(response).to have_http_status(200)
+      expect(response).to render_template('show')
+    end
+
+    it 'assigns the category' do
+      get :show, params: { id: tiket.id }
+      expect(assigns(:tiket)).to eq(tiket)
+    end
+  end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,6 +8,8 @@ require File.expand_path('../config/environment', __dir__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 require 'shoulda/matchers'
+
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -38,6 +40,7 @@ RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   config.formatter = :documentation
+  config.include FactoryBot::Syntax::Methods
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
